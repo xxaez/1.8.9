fe38398acda0f8f46a9db8f017d46d0b381173da
GitHub Action <action@github.com>
2025-03-27 13:43:51 +0000

    Initialize template repository proper
    Set package name to com.github.xxaez.189
    Set project name to 1.8.9
    Set modid to

--- src/main/java/com/github/xxaez/189/ExampleMod.java
+++ src/main/java/com/github/xxaez/189/ExampleMod.java
@@ -1,11 +1,11 @@
-package com.example;
+package com.github.xxaez.189;
 
 import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.init.Blocks;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 
-@Mod(modid = "examplemod", useMetadata=true)
+@Mod(modid = "", useMetadata=true)
 public class ExampleMod {
     @Mod.EventHandler
     public void init(FMLInitializationEvent event) {
--- src/main/java/com/github/xxaez/189/mixin/MixinGuiMainMenu.java
+++ src/main/java/com/github/xxaez/189/mixin/MixinGuiMainMenu.java
@@ -1,4 +1,4 @@
-package com.example.mixin;
+package com.github.xxaez.189.mixin;
 
 import net.minecraft.client.gui.GuiMainMenu;
 import org.spongepowered.asm.mixin.Mixin;
